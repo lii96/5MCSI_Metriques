ec2d006f038f0534c556151b4ab7389196978c06
Lynda Ziane <51752512+lii96@users.noreply.github.com>
2024-09-12 16:46:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,20 +4,16 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+
 app = Flask(__name__)                                                                                                                  
+
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>" #page contact
-
-
-
-
-
+    return "<h2>Ma page de contact</h2>"
 
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
   
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True)                                                                 
