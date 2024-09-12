f4160185a0680ec2fa1547092ef0921b5a29464b
Lynda Ziane <51752512+lii96@users.noreply.github.com>
2024-09-12 17:09:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,9 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contact():
+    return render_template('contact.html')
+
 #Exo 3
 @app.route('/tawarano/')
 def meteo():
