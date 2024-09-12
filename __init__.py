7806f17c66846f10c98d89c7c22dc77b845c1ee3
Lynda Ziane <51752512+lii96@users.noreply.github.com>
2024-09-12 16:19:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,6 +22,24 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results) #TAWARANO
 
+
+@app.route('/histogramme/')
+def afficher_histogramme():
+    return render_template('histogramme.html')
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results) #HISTOGRAMME
+
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html") #GRAPHIC
