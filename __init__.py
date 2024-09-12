29d94c686357807cd032dc3853f2579cbf70ac41
Lynda Ziane <51752512+lii96@users.noreply.github.com>
2024-09-12 17:06:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,6 +27,19 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+    #histogramme 
+@app.route('/histogramme/')
+def histogramme():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = datetime.utcfromtimestamp(list_element.get('dt')).strftime('%Y-%m-%d %H:%M:%S')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
+        results.append([dt_value, temp_day_value])
+    return render_template('histogramme.html', results=results) 
+    #fin histgramme
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
